73edc3552107ac111b9e5c24a16b7b1102e60aab
Levir-Hernandez <145631783+Levir-Hernandez@users.noreply.github.com>
2024-09-18 12:18:14 -0500

    Update create-queue.sh

--- Taller 1 - RabbitMQ/version-java-compose/broker/create-queue.sh
+++ Taller 1 - RabbitMQ/version-java-compose/broker/create-queue.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 set -e
-
 # Espera a que RabbitMQ esté completamente disponible
 until curl -s -f -u guest:guest http://localhost:15672/api/overview > /dev/null; do
-  echo "Waiting for RabbitMQ to be available..."
-  sleep 1
+	echo "Waiting for RabbitMQ to be available..."
+	sleep 1
 done
-
 # Ejecuta el script de creación de colas
 curl -u guest:guest -H "Content-Type: application/json" -X PUT -d '{"durable":true}' http://localhost:15672/api/queues/%2f/cola1
