6c15fcdde9ce4496454668b1f2da11182089cfa5
Levir-Hernandez <145631783+Levir-Hernandez@users.noreply.github.com>
2024-09-18 15:26:51 -0500

    Add files via upload
    5

--- Taller 1 - RabbitMQ/version-java-compose/broker/kreate-queue.sh
+++ Taller 1 - RabbitMQ/version-java-compose/broker/kreate-queue.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 set -e
-rabbit-server &
+
+if [-z "$1"]; then
+	echo "Error: No queue name provided."
+	echo "Usage: $0 <queue_name>"
+	exit 1
+fi
+
 until curl -s -f -u guest:guest http://localhost:15672/api/overview > /dev/null; do
 	echo "Waiting for RabbitMQ to be available..."
 	sleep 1
 done
 echo "RabbitMQ is now available."
-curl -u guest:guest -H "Content-Type: application/json" -X PUT -d '{"durable":true}' http://localhost:15672/api/queues/%2f/cola1
-echo 'The queue "cola1" was created successfully.'
-wait
+
+curl -u guest:guest -H "Content-Type: application/json" -X PUT -d '{"durable":true}' http://localhost:15672/api/queues/%2f/$1
+echo "The queue \"$1\" was created successfully."
