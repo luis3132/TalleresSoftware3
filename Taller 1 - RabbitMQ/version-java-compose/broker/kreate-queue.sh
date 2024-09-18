475829cac1bd5bf122e15c7c74dad5b64d6f3e98
Levir-Hernandez <145631783+Levir-Hernandez@users.noreply.github.com>
2024-09-18 12:37:43 -0500

    Add files via upload
    code with LF

---
+++ Taller 1 - RabbitMQ/version-java-compose/broker/kreate-queue.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+until curl -s -f -u guest:guest http://localhost:15672/api/overview > /dev/null; do
+	echo "waiting..."
+	sleep 1
+done
+curl -u guest:guest -H "Content-Type: application/json" -X PUT -d '{"durable":true}' http://localhost:15672/api/queues/%2f/cola1
