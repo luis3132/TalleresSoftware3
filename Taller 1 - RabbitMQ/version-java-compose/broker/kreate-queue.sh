98162ce4016418f5a9470d4fdd6afbd72ccce183
Levir-Hernandez <145631783+Levir-Hernandez@users.noreply.github.com>
2024-09-18 12:52:55 -0500

    Add files via upload
    2

--- Taller 1 - RabbitMQ/version-java-compose/broker/kreate-queue.sh
+++ Taller 1 - RabbitMQ/version-java-compose/broker/kreate-queue.sh
@@ -5,3 +5,4 @@ until curl -s -f -u guest:guest http://localhost:15672/api/overview > /dev/null;
 	sleep 1
 done
 curl -u guest:guest -H "Content-Type: application/json" -X PUT -d '{"durable":true}' http://localhost:15672/api/queues/%2f/cola1
+echo "done"
