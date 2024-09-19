c3fd05130c951d9599bb3d3bac2f7f51f151325c
Levir-Hernandez <145631783+Levir-Hernandez@users.noreply.github.com>
2024-09-18 20:55:55 -0500

    Delete Taller 1 - RabbitMQ/version-java-compose/broker directory

--- Taller 1 - RabbitMQ/version-java-compose/broker/kreate-queue.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-rabbitmq-server &
-while ! nc -z localhost 15672; do
-	echo "Waiting for RabbitMQ..."
-	sleep 1
-done
-rabbitmqadmin -u guest -p guest -H localhost -P 15672 declare queue name=$QUEUE_NAME durable=true
-wait
