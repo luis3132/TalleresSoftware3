8de99dcff5f1ee011b7418e724ae3090b8eba152
Levir Hernandez <levir_h@hotmail.com>
2024-09-18 20:58:02 -0500

    compose funcional v1 y v2 en java (misma tecnologia)

---
+++ Taller 1 - RabbitMQ/version-java-compose/producer/src/main/java/com/rabbitmq/producer/ProducerService.java
@@ -0,0 +1,20 @@
+package com.rabbitmq.producer;
+
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableRabbit
+public class ProducerService
+{
+    @Autowired private RabbitTemplate rabbitTemplate;
+    @Value("${queue.name}") private String queue;
+
+    public void publish(String message)
+    {
+        rabbitTemplate.convertAndSend(queue, message);
+    }
+}
